5466538de4cc213328802d28e68cd461415b95af
jenkin <hnyangcz@126.com>
2022-09-27 01:01:11 +0800

    教资003

--- netty-nio/src/main/java/com/jenkin/netty/nio/performance/Test.java
+++ netty-nio/src/main/java/com/jenkin/netty/nio/performance/Test.java
@@ -29,6 +29,9 @@ public class Test {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             int hashCode = nextHashCode() & 15;
+
+
+
             System.out.println(hashCode);
         }
     }
